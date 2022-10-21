f6563f4e5ed15168f77b400dd466615579843987
samanthamarberger <slatham04@gmail.com>
2022-10-20 22:17:40 -0600

    commit tree 90950b87286080ec3701acb7a0b29b08e17a32d1

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let palindrome = false;
+  const arrayForm = Array.from(word);
+  const manipulatedArray = [...arrayForm];
+  const reverseArray = manipulatedArray.reverse();
+  
+  for (let i = 0; i < arrayForm.length; i++){
+    if(arrayForm[i] === reverseArray[i]) {
+      palindrome = true;
+    }
+    else {
+      palindrome = false;
+      break;
+    }
+  }
+  return palindrome;
 }
 
 /* 
   Add your pseudocode here
+
+  create a boolean variable 
+  split the word into an array 
+  if array = array reversed 
+    true
+  if array != array reversed
+    false
+  return boolean 
 */
 
 /*
   Add written explanation of your solution here
+  use aeeay.prototype.reverse() on array to reverse
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,10 +40,14 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("aibophpobia"))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
 }
 
 module.exports = isPalindrome;
